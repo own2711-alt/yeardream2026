6474e1e548204edf83acde4281379a0dd34ce5c3
own2711-alt <own2711@gmail.com>
2026-08-28 11:49:52 +0900

    model schema 완성

--- express/05_mongodb/member_router.js
+++ express/05_mongodb/member_router.js
@@ -1,31 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-// 회원 가입 (/member/join)
-router.post('/join', (req, res) => {
-    res.json({'success': true,'data':'회원가입 완료'});
+// 회원 가입(/member/join)
+router.post('/join',function(req,res){
+    const param = req.body;
+    res.json({'success':true,'data':param});
 });
 
-// 회원 리스트 (/member/list, /member)
-router.get(['/list','/'], (req, res) => {
-    res.json({'success': true,'data':[]});
-}); // <--- 여기 괄호 오타 수정완료!
+// 회원 리스트(/member/list, /memeber/)
+router.get(['/list','/'],function(req,res){
+    res.json({'success':true,'data':[]});
+});
 
-// 회원정보 상세보기 (/member/get/:id)
-router.get('/get/:id', (req, res) => {
+// 회원정보 상세보기(/member/get/:id)
+router.get('/get/:id',function(req,res){
     const {id} = req.params;
-    res.json({'success': true,'data':{'id':id,'msg':'상세보기 완료'}});
+    res.json({'success':true,'data':{'id':id,'msg':'상세보기 완료'}});
 });
 
-// 회원정보 수정 (/member/update/:id)
-router.put('/update/:id', (req, res) => {
+// 회원정보 수정(/member/update/:id)
+router.put('/update/:id',function(req,res){
     const {id} = req.params;
-    res.json({'success': true,'data':{'id':id,'msg':'수정 완료'}});
+    const param = req.body;
+    res.json({'success':true,'data':{'id':id,'msg':param}});
 });
 
-// 회원 삭제 (/member/delete/:id)
-router.delete('/delete/:id', (req, res) => {
-    res.json({'success': true,'data':'회원삭제 완료'});
+// 회원 삭제(/member/delete/:id)
+router.delete('/delete/:id',function(req,res){
+    res.json({'success':true,'data':'회원삭제 완료'});
 });
 
 module.exports = router;
---
+++ express/05_mongodb/model.js
@@ -0,0 +1,47 @@
+// mongoose 는 스키마 설정이 가능하다. (테이블 형태)
+const mongoose = require('mongoose');
+
+let schema = new mongoose.Schema({
+    id:{
+        type:String,
+        required:[true,'아이디는 필수 입니다.'],
+        unique:true, // 중복허용안함
+        trim:true,     //  앞뒤 공백 잘라서 가져옴
+        minlength:[4,'아이디는 4자 이상 입니다.'],
+        maxLength:[25,'아이디는 25자 이하 입니다.']
+    },
+    pw:{
+        type:String,
+        required: [true,'비밀번호는 필수 입니다.'],
+        trim:true,
+        select:false // 조회할때 기본적으로 빼고 가져온다.
+    },
+    name:{
+        type:String,
+        required:[true,'이름은 필수 입니다.'],
+        trim:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:[true,'전화번호는 필수 입니다.'],
+        unique:true,
+        trim:true
+    },
+    grade:{
+        type:String,
+        default:'user',
+        enum:['user','admin']
+    }
+},{
+    collection:'member', // 적용할 컬렉션
+    timestamps:true, // 특정 액션이 일어난 요일,시간을 기록하는 기능
+    // mogo db 는 기본적으로 _id 를 만든다.
+    // mongoose 에서는 이 _id 를 id 로 기록한다.
+    // 이 기능을 껐다.
+    id:false
+});
+
+schema.index({name:1});
+// model명은 단수형 파스칼 표기법을 사용 한다.
+module.exports = mongoose.model('Member',schema);
