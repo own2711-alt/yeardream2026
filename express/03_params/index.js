741f5c77b0c3fa9db9d3b2657c66bc27016f8030
own2711-alt <own2711@gmail.com>
2026-08-28 09:33:54 +0900

    parmas 마무리

--- express/03_params/index.js
+++ express/03_params/index.js
@@ -3,23 +3,35 @@ const app = express();
 
 //GET /rest/admin/pass
 app.get('/rest/:id/:pw',function(req,res){
-    console.log(req.params); // get에서만 사용가능 post 에서는 사용 불가
-    // const id = req.params.id;
+    console.log(req.params); // post 에서는 이걸로 받을수 없다.
+    //const id = req.params.id;
     const {id,pw} = req.params;
-    res.json({'msg': '잘 받았음','params':{id,pw}}); //params : {id:id, pw:pw}
+    res.json({
+        'msg':'잘 받았음',
+        'params':{id, pw} // params : {id:id, pw:pw}
+    });
 });
 
 //GET /get_method?id=admin&pw=pass
 app.get('/get_method',function(req,res){
     console.log(req.query);
-    const {id,pw}  = req.params;
-    res.send({id,pw});
-})
+    const {id,pw} = req.query;
+    res.json({'params':{id,pw}});
+});
 
 //POST /login
 // {id:"admin",pw:"pass"}
-app.post('/post',function(req,res){})
+// request 의 body 에 JSON 형태의 데이터를 받을때 (예) Axios
+app.use(express.json());
+app.post('/login',function(req,res){
+    console.log('body',req.body);
+    const {id,pw} = req.body;
+    res.json({'body':{id,pw}});
+});
 
 // 위 URL 외의 것이 왔을때 처리
+app.use('/*path',function(req,res){
+    res.send('잘못된 요청 입니다.');
+});
 
 app.listen(80,()=>console.log('http://localhost'));
